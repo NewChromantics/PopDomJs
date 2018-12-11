4cdb894a789eade0aa3b4327275163f727e6652b
Graham Reeves <graham@grahamreeves.com>
2018-12-11 15:19:57 +0000

    Added mouse interaction

--- PopDomJs.js
+++ PopDomJs.js
@@ -1,3 +1,8 @@
+Math.Inside01 = function(x,y)
+{
+	return ( x>=0 && x<=1 && y>=0 && y<=1 );
+}
+
 //	uses PopMath for Rect(x,y,w,h)
 function TRect(x,y,w,h)
 {
@@ -10,7 +15,17 @@ function TRect(x,y,w,h)
 	{
 		return [this.x,this.y,this.w,this.h];
 	}
+
+	this.GetLeft = function()
+	{
+		return this.x;
+	}
 	
+	this.GetTop = function()
+	{
+		return this.y;
+	}
+
 	this.GetBottom = function()
 	{
 		return this.y + this.h;
@@ -31,6 +46,13 @@ function TRect(x,y,w,h)
 		let Bottom = Math.max( Rect.GetBottom(), this.GetBottom() );
 		this.h = Bottom - this.y;
 	}
+	
+	this.GetLocalUv = function(Uv)
+	{
+		let u = Math.Range( this.GetLeft(), this.GetRight(), Uv.x );
+		let v = Math.Range( this.GetTop(), this.GetBottom(), Uv.y );
+		return new float2(u,v);
+	}
 }
 
 
@@ -43,6 +65,7 @@ function PopDom(OnChanged)
 	//	todo: tree!
 	this.Elements = [];
 	this.OnChanged = OnChanged;
+	this.LockedElement = null;		//	mousedown started on this element
 	
 	
 	this.GetElement = function(Name)
@@ -83,6 +106,71 @@ function PopDom(OnChanged)
 		this.OnChanged();
 		return this.Elements[Name];
 	}
+	
+	//	todo: make a proper/generic "background" control?
+	this.BackgroundElement = {};
+	this.BackgroundElement.Rect = new TRect(0,0,1,1);
+	this.BackgroundElement.Control = {};
+	this.BackgroundElement.Control.OnClick = function(uv,FirstClick,Button){}
+	this.BackgroundElement.Control.OnHover = function(uv){}
+	this.BackgroundElement.Control.OnScroll = function(ScrollHv,uv){}
+	
+
+	this.GetElementAt = function(u,v,UseLocked)
+	{
+		UseLocked = (UseLocked!==false);	//	default to true
+		let ResultElement = null;
+		let uv = new float2(u,v);
+		
+		let MatchElement = function(Element,Force)
+		{
+			Force = (Force===true);
+			let LocalUv = Element.Rect.GetLocalUv( uv );
+			if ( !Force && !Math.Inside01( LocalUv.x, LocalUv.y ) )
+				return;
+			ResultElement = {};
+			ResultElement.Element = Element;
+			ResultElement.LocalUv = LocalUv;
+		}
+		
+		if ( UseLocked && this.LockedElement )
+		{
+			MatchElement( this.LockedElement, true );
+		}
+		else
+		{
+			this.EnumElements( MatchElement );
+		}
+		if ( !ResultElement )
+			MatchElement( this.BackgroundElement, true );
+		
+		return ResultElement;
+	}
+	
+	
+	this.OnMouseDown = function(u,v)
+	{
+		this.LockedElement = null;
+		let MatchElement = this.GetElementAt( u, v );
+		this.LockedElement = MatchElement.Element;
+		MatchElement.Element.Control.OnClick( MatchElement.LocalUv, true, PopFragGui.ButtonLeft );
+	}
+
+	this.OnMouseUp = function(u,v)
+	{
+		this.LockedElement = null;
+	}
+
+	this.OnMouseMove = function(u,v)
+	{
+		//	mouse down
+		let MatchElement = this.GetElementAt( u, v );
+		if ( this.LockedElement )
+			MatchElement.Element.Control.OnClick( MatchElement.LocalUv, false, PopFragGui.ButtonLeft );
+		else
+			MatchElement.Element.Control.OnHover( MatchElement.LocalUv );
+	}
+
 	
 }
 
